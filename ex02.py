e3ae4c6d863ddce12f0c5f571c58f18b07de5110
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2023-07-28 22:43:35 +0530

    selenium example 02

--- ex02.py
+++ ex02.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.by import By
 service = Service(executable_path = "./drivers/geckodriver",
                  log_path = "./logs/geckodriver.log"
                  );
-
 browser = webdriver.Firefox(service=service);
+
 browser.get("https://www.random.org/strings/?num=1&len=20&digits=on&upperalpha=on&loweralpha=on&unique=on&format=html&rnd=new");
 assert "String Generator" in browser.title;
 
