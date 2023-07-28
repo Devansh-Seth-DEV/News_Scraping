d523fc2d409d472b3c5d5e0d2501152fc73f0427
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2023-07-28 22:44:01 +0530

    selenium example 03

--- ex03.py
+++ ex03.py
@@ -6,8 +6,8 @@ from selenium.webdriver.common.by import By
 service = Service(executable_path = "./drivers/geckodriver",
                   log_path = "./logs/geckodriver.log"
                   );
-
 browser = webdriver.Firefox(service=service);
+
 browser.get("http://random-name-generator.info/");
 
 nameList = browser.find_elements(By.CSS_SELECTOR, "ol.nameList li");
