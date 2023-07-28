f933da31b7d793a0d65484257ef3d3f1b990e35c
Devansh-Seth-DEV <dev.devanshseth.engg0072@gmail.com>
2023-07-28 23:17:58 +0530

    moved examples to seperate folder

---
+++ selenium_basic_examples/ex03.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+service = Service(executable_path = "./drivers/geckodriver",
+                  log_path = "./logs/geckodriver.log"
+                  );
+browser = webdriver.Firefox(service=service);
+
+browser.get("http://random-name-generator.info/");
+
+nameList = browser.find_elements(By.CSS_SELECTOR, "ol.nameList li");
+
+print("NAME LIST: ");
+for name in nameList:
+    print("%s"%name.text);
+
+browser.close();
